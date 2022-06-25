93a89697323a3fc938e0026d4398a024c21b4dc2
ldickmanns <ludwig.dickmanns@outlook.de>
2022-06-25 13:26:30 +0200

    Added code and resources.

---
+++ app/src/main/java/com/ldickmanns/gifapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.ldickmanns.gifapp
+
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CoroutineScope(SupervisorJob()).launch(Dispatchers.IO)  {
+            val source = ImageDecoder.createSource(
+                resources, R.drawable.kotlin_android
+            )
+            val drawable = ImageDecoder.decodeDrawable(source)
+
+            val imageView = findViewById<ImageView>(R.id.image_view)
+            imageView.post {
+                imageView.setImageDrawable(drawable)
+                (drawable as? AnimatedImageDrawable)?.start()
+            }
+        }
+    }
+}
